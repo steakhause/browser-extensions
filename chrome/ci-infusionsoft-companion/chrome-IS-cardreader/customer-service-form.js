9f93419e449e546774c404c89b88636e63f91cb2
jryszka <jason@cleverinvestor.com>
2022-10-27 15:59:21 -0700

    additional utility links

--- chrome/ci-infusionsoft-companion/chrome-IS-cardreader/customer-service-form.js
+++ chrome/ci-infusionsoft-companion/chrome-IS-cardreader/customer-service-form.js
@@ -1,9 +1,19 @@
-jQuery(document).ready(function(){
+jQuery(document).ready(function () {
+    const urlParams = new URLSearchParams(window.location.search);
+    const param_x = urlParams.get('param_x');
+    var contactId = urlParams.get('ID');
+    var fullName = jQuery('.page-header-user h2').html();
+
+    var linkElement = '<li class="tab-blur" id="tab_main" nowrap="nowrap"><a class="tab-blur" id="tab_link_main" href="https://api.cleverinvestor.com/dealautomator/utility/?infusion_id=' + contactId + '" target="_blank">DA Utility</a></li>';
+    linkElement += '<li class="tab-blur" id="tab_main" nowrap="nowrap"><a class="tab-blur" id="tab_link_main" href="https://api.cleverinvestor.com/infusionsoft/dup_finder.php?contactId=' + contactId + '" target="_blank">Dup Check</a></li>';
+    linkElement += '<li class="tab-blur" id="tab_main" nowrap="nowrap"><a class="tab-blur" id="tab_link_main" href="https://api.cleverinvestor.com/registration/ioe/?contactId=' + contactId + '&email=' + fullName + '" target="_blank">Registration</a></li>';
+
+    jQuery('#pagecontent .tabs').first().append(linkElement);
     jQuery('#FormId_select').prepend('<option val="cscf">Customer Service Utilities</option>');
 
-    jQuery('#FormId_select').change(function(){
-        if( jQuery(this).val() == 'Customer Service Utilities') {
-            jQuery('body').prepend(' <iframe src="https://api.cleverinvestor.com/dealautomator/utility/" height="1080" width="1920" style="position: absolute; top: 0; bottom: 0; left: 0; right: 0; height: 100%; width: 100%; z-index: 51;" title="Iframe Test"></iframe> ');
+    jQuery('#FormId_select').change(function () {
+        if (jQuery(this).val() == 'Customer Service Utilities') {
+            window.open('https://api.cleverinvestor.com/dealautomator/utility/?infusion_id=' + contactId, '_blank');
         }
     });
 });

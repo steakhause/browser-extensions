27a1bfdb483cd78e7c2f35d8daf26638982890d8
jryszka <jason@cleverinvestor.com>
2023-01-13 09:51:50 -0700

    Ultimate Coaching utilities for Firefox

--- chrome/ci-infusionsoft-companion/is-add-on/contact-links.js
+++ chrome/ci-infusionsoft-companion/is-add-on/contact-links.js
@@ -20,4 +20,40 @@ jQuery(document).ready(function () {
             window.open('https://api.cleverinvestor.com/infusionsoft/form/customer-service-communication/?infusion_id=' + contactId + '&uid=' + userId, '_blank');
         }
     });
+
+    /**
+     * Ultimate Coaching
+     * End Date Adjustment
+     * Pause & Resume Program
+     * Active & Graduated Badges
+     */
+
+    jQuery('#tab_data_custom_Custom_Fields_1').on('mouseover mousemove scroll', function () {
+        /*Load Pause & Resume buttons.*/
+        if (!jQuery('#pauseUCProg').length) {
+            jQuery('#Contact0_CoachingEndDate_data').parent().css({ 'border': '1px solid #5e5e5e', 'vertical-align': 'middle' }).append('<td style="vertical-align:middle; padding-right: 6px"><button id="pauseUCProg" class="cleverAjaxTrigger" type="button" style="min-width: max-content;">Pause Program</button></td><td style="vertical-align:middle; padding-right: 6px"><button id="resumeUCProg" class="cleverAjaxTrigger" type="button" style="min-width: max-content;">Resume Program</button></td>');
+        }
+    });
+
+    jQuery('.cleverAjaxTrigger').on('click', function () {
+        alert('clicked');
+        const urlParams = new URLSearchParams(window.location.search);
+        const param_x = urlParams.get('param_x');
+        var contactId = urlParams.get('ID');
+        var id = jQuery(this).attr('id');
+        switch (id) {
+            case 'pauseUCProg':
+            case 'resumeUCProg':
+                jQuery.post("https://api.cleverinvestor.com/infusionsoft/api_goal.php",
+                    {
+                        key: 'KpKl3l5krK1K$VGH5ZzYw6iT',
+                        contactId: contactId,
+                        integration: "pn120",
+                        eventCode: id
+                    },
+                    function (data, status) {
+                        alert("Data: " + data + "\nStatus: " + status);
+                    });
+        }
+    });
 });

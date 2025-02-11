6e19f48df9dd9494f6f82e46a4f1faa5afdf878b
Jason Ryszka <jason.ryszka@gmail.com>
2025-02-11 12:15:44 -0700

    Updated to nexus.cleverinvestor.com

--- chrome/ci-infusionsoft-companion/is-add-on/contact-links.js
+++ chrome/ci-infusionsoft-companion/is-add-on/contact-links.js
@@ -4,30 +4,11 @@ jQuery(document).ready(function () {
     var contactId = urlParams.get('ID');
     var fullName = jQuery('.page-header-user h2').html().replace("<div>", "").replace("</div>", "");
 
-    var linkElement = '<li class="tab-blur" id="tab_main" nowrap="nowrap"><a class="tab-blur" id="tab_link_main" href="https://api.cleverinvestor.com/dealautomator/utility/?infusion_id=' + contactId + '" target="_blank">DA Utility</a></li>';
-    linkElement += '<li class="tab-blur" id="tab_main" nowrap="nowrap"><a class="tab-blur" id="tab_link_main" href="https://api.cleverinvestor.com/infusionsoft/dup_finder.php?contactId=' + contactId + '" target="_blank">Dup Check</a></li>';
-    linkElement += '<li class="tab-blur" id="tab_main" nowrap="nowrap"><a class="tab-blur" id="tab_link_main" href="https://api.cleverinvestor.com/registration/ioe/?contactId=' + contactId + '&email=' + fullName + '" target="_blank">Registration</a></li>';
+    var linkElement = '<li class="tab-blur" id="tab_main" nowrap="nowrap"><a class="tab-blur" id="tab_link_main" href="https://nexus.cleverinvestor.com/utilities/deal-automator/?infusion_id=' + contactId + '" target="_blank">DA Utility</a></li>';
+    linkElement += '<li class="tab-blur" id="tab_main" nowrap="nowrap"><a class="tab-blur" id="tab_link_main" href="https://nexus.cleverinvestor.com/utilities/infusionsoft-duplicate-contact-search/?contactId=' + contactId + '" target="_blank">Dup Check</a></li>';
 
     jQuery('#pagecontent .tabs').first().append(linkElement);
     jQuery('#FormId_select').prepend('<option val="cscf">Customer Service Utilities</option>');
-
-    jQuery('#FormId_select').change(function () {
-        var script = jQuery('#grid_div_GridFileBoxContact script').html();
-        var start = script.indexOf('id=') + 3;
-        var end = script.indexOf('&delete=');
-        var userId = script.substr(start, end - start);
-        if (jQuery(this).val() == 'Customer Service Utilities') {
-            window.open('https://api.cleverinvestor.com/infusionsoft/form/customer-service-communication/?contactId=' + contactId + '&uid=' + userId, '_blank');
-        }
-    });
-
-    /**
-    * Ultimate Coaching
-    * End Date Adjustment
-    * Pause & Resume Program
-    * Active & Graduated Badges
-    */
-
 
     var currEndDate = '';
 
@@ -64,7 +45,7 @@ jQuery(document).ready(function () {
 
         }
         if(goalName != ''){
-            jQuery.post("https://api.cleverinvestor.com/infusionsoft/api_goal.php",
+            jQuery.post("https://nexus.cleverinvestor.com/infusionsoft/api_goal.php",
                 {
                     key: 'KpKl3l5krK1K$VGH5ZzYw6iT',
                     contactId: contactId,
--- firefox/is-add-on/order-links.js
+++ firefox/is-add-on/order-links.js
@@ -3,7 +3,7 @@ jQuery(document).ready(function () {
     var p = l.indexOf('ID=',l) + 3;
     var contactId = l.substring(p, l.length);
 
-    var linkElement = '<input type="button" value="Update IOE Sheet" class="inf-button" onclick="window.open(\'https://api.cleverinvestor.com/infusionsoft/api_goal.php?contactId=' + contactId + '&integration=IOE&eventCode=Nov2022Sheet\', \'IOE Sheet\', \'toolbars=0,width=300,height=300,left=200,top=200,scrollbars=1,resizable=1\')">';
+    var linkElement = '<input type="button" value="Update IOE Sheet" class="inf-button" onclick="window.open(\'https://nexus.cleverinvestor.com/infusionsoft/api_goal.php?contactId=' + contactId + '&integration=IOE&eventCode=Nov2022Sheet\', \'IOE Sheet\', \'toolbars=0,width=300,height=300,left=200,top=200,scrollbars=1,resizable=1\')">';
 
     jQuery('#Refund_Payment').parent().append(linkElement);
 });
